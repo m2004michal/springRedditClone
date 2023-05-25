1e92b708ecca329637462a49aba0f5eb83bddefc
m2004michal <michal.tusz12@gmail.com>
2023-05-25 18:53:17 +0200

    deleted some comments

--- src/main/java/com/example/redditClone/security/JwtProvider.java
+++ src/main/java/com/example/redditClone/security/JwtProvider.java
@@ -1,7 +1,5 @@
 package com.example.redditClone.security;
 
-import com.example.redditClone.runtimeException.SpringRedditException;
-import io.jsonwebtoken.Jwts;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,47 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.*;
-import java.security.cert.CertificateException;
-import java.time.Instant;
-
-//@Service
-//public class JwtProvider {
-//
-//    private KeyStore keyStore;
-//
-//    @PostConstruct
-//    public void init() {
-//        try {
-//            keyStore = KeyStore.getInstance("JKS");
-//            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog2.jks");
-//            keyStore.load(resourceAsStream, "secret".toCharArray());
-//        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
-//            throw new SpringRedditException("Exception occurred while loading keystore");
-//        }
-//
-//    }
-//
-//    public String generateToken(Authentication authentication) {
-//        org.springframework.security.core.userdetails.User principal = (User) authentication.getPrincipal();
-//        return Jwts.builder()
-//                .setSubject(principal.getUsername())
-//                .signWith(getPrivateKey())
-//                .compact();
-//    }
-//
-//    private PrivateKey getPrivateKey() {
-//        try {
-//            return (PrivateKey) keyStore.getKey("springblog2", "secret".toCharArray());
-//        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
-//            e.printStackTrace();
-//            throw new SpringRedditException("Exception occured while retrieving public key from keystore " );
-//        }
-//    }
-//}
 @Service
 @RequiredArgsConstructor
 public class JwtProvider {
